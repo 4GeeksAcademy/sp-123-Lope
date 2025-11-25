e1aedb25dca529fb910f9e7525ff66baaeff4c22
Lope MS <lopems@gmail.com>
2025-11-25 14:22:19 +0000

    Inicio: Se eliminó la parte original del proyecto para iniciar el proyecto de Lope. Se creo nuevo navbar.

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -22,9 +22,10 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+        
       </Route>
     )
 );
